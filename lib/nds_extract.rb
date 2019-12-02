d4ac653e1417ec94e5e2dffbd43ba13908605b68
Anna Dybas <a.m.dybas@gmail.com>
2019-12-02 21:40:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,25 +7,19 @@ def directors_totals(nds)
 
   while directors_index < directors_database.length do
     movie_title = 0
-    while movie_title < directors_database[directors_index][:movies].length do
-
+    total_gross = 0
+    while movie_title < directors_database[directors_index][:movies].length do 
+       total_gross += directors_database[directors_index][:movies][movie_title][:worldwide_gross]
       movie_title += 1
     end
+    results {directors_database[directors_index][:name]=> total_gross}
     directors_index += 1
   end
   result
 end
 
-def print_first_directors_movie_titles
-  count = 0
-  while count < directors_database[0][:movies].length do #while count is less than the length of our first element
-      puts "#{directors_database[0][:movies][count][:title]}" #we outputs
-      count += 1 #and then we add one to count
-    end
-end
-
-directors_database[directors_index][:name]
+#directors_database[directors_index][:name]
 #"Stephen Spielberg"=>1357566430,
 #"Russo Brothers"=>2281002470,
 
-directors_database[directors_index][:movies][movie_title][:worldwide_gross]
+#directors_database[directors_index][:movies][movie_title][:worldwide_gross]
